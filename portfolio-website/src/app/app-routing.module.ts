094f88b0d4b797d1377ffec39ce50efb95feea65
dan933 <69749413+dan933@users.noreply.github.com>
2022-01-29 13:54:35 +1100

    created components folder to clean up code

--- portfolio-website/src/app/app-routing.module.ts
+++ portfolio-website/src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { OverviewComponent } from './overview/overview.component';
-import { RProjectsComponent } from './r-projects/r-projects.component';
-import { CProjectsPictureFilterComponent } from './c-projects/c-projects-picture-filter/c-projects-picture-filter.component';
-import { CProjectsCreditComponent } from './c-projects/c-projects-credit/c-projects-credit.component';
-import { CProjectsSubstitutionComponent } from './c-projects/c-projects-substitution/c-projects-substitution.component';
-import { CProjectsRecoverComponent } from './c-projects/c-projects-recover/c-projects-recover.component';
-import { CProjectsSpellerComponent } from './c-projects/c-projects-speller/c-projects-speller.component';
+import { OverviewComponent } from './components/overview/overview.component';
+import { RProjectsComponent } from './components/r-projects/r-projects.component';
+import { CProjectsPictureFilterComponent } from './components/c-projects/c-projects-picture-filter/c-projects-picture-filter.component';
+import { CProjectsCreditComponent } from './components/c-projects/c-projects-credit/c-projects-credit.component';
+import { CProjectsSubstitutionComponent } from './components/c-projects/c-projects-substitution/c-projects-substitution.component';
+import { CProjectsRecoverComponent } from './components/c-projects/c-projects-recover/c-projects-recover.component';
+import { CProjectsSpellerComponent } from './components/c-projects/c-projects-speller/c-projects-speller.component';
 
-import { PythonProjectsComponent } from './python-projects/python-projects.component';
-import { SqlProjectsComponent } from './sql-projects/sql-projects.component';
+import { PythonProjectsComponent } from './components/python-projects/python-projects.component';
+import { SqlProjectsComponent } from './components/sql-projects/sql-projects.component';
 
 const routes: Routes = [
   { path: 'overview', component: OverviewComponent },
--- portfolio-website/src/app/app.module.ts
+++ portfolio-website/src/app/app.module.ts
@@ -7,15 +7,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //angular materials module
 import { AppMaterialModule } from './app-material/app-material.module';
-import { OverviewComponent } from './overview/overview.component';
-import { RProjectsComponent } from './r-projects/r-projects.component';
-import { CProjectsPictureFilterComponent } from './c-projects/c-projects-picture-filter/c-projects-picture-filter.component';
-import { CProjectsCreditComponent } from './c-projects/c-projects-credit/c-projects-credit.component';
-import { CProjectsSubstitutionComponent } from './c-projects/c-projects-substitution/c-projects-substitution.component';
-import { CProjectsRecoverComponent } from './c-projects/c-projects-recover/c-projects-recover.component';
-import { CProjectsSpellerComponent } from './c-projects/c-projects-speller/c-projects-speller.component';
-import { PythonProjectsComponent } from './python-projects/python-projects.component';
-import { SqlProjectsComponent } from './sql-projects/sql-projects.component';
+import { OverviewComponent } from './components/overview/overview.component';
+import { RProjectsComponent } from './components/r-projects/r-projects.component';
+import { CProjectsPictureFilterComponent } from './components/c-projects/c-projects-picture-filter/c-projects-picture-filter.component';
+import { CProjectsCreditComponent } from './components/c-projects/c-projects-credit/c-projects-credit.component';
+import { CProjectsSubstitutionComponent } from './components/c-projects/c-projects-substitution/c-projects-substitution.component';
+import { CProjectsRecoverComponent } from './components/c-projects/c-projects-recover/c-projects-recover.component';
+import { CProjectsSpellerComponent } from './components/c-projects/c-projects-speller/c-projects-speller.component';
+import { PythonProjectsComponent } from './components/python-projects/python-projects.component';
+import { SqlProjectsComponent } from './components/sql-projects/sql-projects.component';
 
 @NgModule({
   declarations: [
