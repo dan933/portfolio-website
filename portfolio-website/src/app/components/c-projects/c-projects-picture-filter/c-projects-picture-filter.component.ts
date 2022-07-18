5b0d0d3d0f63c88b60acfcccbde944e80043aa02
dan933 <69749413+dan933@users.noreply.github.com>
2022-07-18 14:24:13 +1000

    formatting bug fixes

--- portfolio-website/src/app/components/c-projects/c-projects-picture-filter/c-projects-picture-filter.component.ts
+++ portfolio-website/src/app/components/c-projects/c-projects-picture-filter/c-projects-picture-filter.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTabGroup } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-c-projects-picture-filter',
@@ -6,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./c-projects-picture-filter.component.scss']
 })
 export class CProjectsPictureFilterComponent implements OnInit {
+  @ViewChild('matTabGroup', { static: true }) matTabGroup!: MatTabGroup;
   panelOpenState = false;
 
   constructor() { }
+
+  setMatTabGroup() {
+    let ntvEl = this.matTabGroup._elementRef.nativeElement;
+    ntvEl.style.minHeight = ntvEl.clientHeight + 'px';
+  }
+
+  onMatTabChanged() {
+    this.setMatTabGroup();
+  }
+
+
 
   ngOnInit(): void {
   }
