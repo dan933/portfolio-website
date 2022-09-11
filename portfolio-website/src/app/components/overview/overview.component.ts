6409553c2bc6c2f26e157a30e1a55b91b4efb614
dan933 <69749413+dan933@users.noreply.github.com>
2022-09-11 10:50:27 +1000

    added gif images

--- portfolio-website/src/app/components/overview/overview.component.ts
+++ portfolio-website/src/app/components/overview/overview.component.ts
@@ -2,26 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { cardData } from 'src/assets/Data/overview-card-data';
 
 interface ICard {
-  title:string,
-  subtitle:string,
-  img: string,
-  caption: string
-  routeLink: string,
-  gitHubLink?:string,
-  IsGitHubButton:boolean
+  title: string;
+  subtitle: string;
+  img: string;
+  caption: string;
+  routeLink: string;
+  gitHubLink?: string;
+  IsGitHubButton: boolean;
+  IsDemoButton?: boolean;
 }
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.scss']
+  styleUrls: ['./overview.component.scss'],
 })
 export class OverviewComponent implements OnInit {
   cards: ICard[] = cardData;
 
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 }
--- portfolio-website/src/assets/Data/overview-card-data.ts
+++ portfolio-website/src/assets/Data/overview-card-data.ts
@@ -1,64 +1,71 @@
-import { RouterLink } from "@angular/router"
+import { RouterLink } from '@angular/router';
 
 interface ICard {
-  title:string,
-  subtitle:string,
-  img: string,
-  caption: string
-  routeLink: string,
-  gitHubLink?:string,
-  IsGitHubButton:boolean
+  title: string;
+  subtitle: string;
+  img: string;
+  caption: string;
+  routeLink: string;
+  gitHubLink?: string;
+  IsGitHubButton: boolean;
+  IsDemoButton?: boolean;
 }
 
 export const cardData: ICard[] = [
   {
-    title:"NBA App",
-    subtitle:"Stack: React, Sql Server and Entity Framework",
-    img: "../../assets/Images/fullstack/invoice-application/NBA-Application.png",
-    caption: "A full stack application users can login create and manage their NBA dream teams.",
-    routeLink: "/projects/nba-app",
-    gitHubLink:"https://github.com/dan933/2022-NBA-Prediction-Application",
-    IsGitHubButton:true,
+    title: 'NBA App',
+    subtitle: 'Stack: React, Sql Server and Entity Framework',
+    img: '../../assets/Images/overview-images/nba.gif',
+    caption:
+      'A full stack application users can login create and manage their NBA dream teams.',
+    routeLink: '/projects/nba-app',
+    gitHubLink: 'https://github.com/dan933/2022-NBA-Prediction-Application',
+    IsGitHubButton: true,
   },
   {
-    title:"Invoice App",
-    subtitle:"Stack: Angular, Sql Server and Entity Framework",
-    img: "../../assets/Images/fullstack/invoice-application/fullstack.png",
-    caption: "An Invoice application that allows the user to create customers and invoices. The invoices can be downloaded as a pdf and can be marked as paid.",
-    routeLink: "/projects/invoice-app",
-    gitHubLink:"https://github.com/dan933/dna-invoice-application-public",
-    IsGitHubButton:true,
+    title: 'RideShare App',
+    subtitle: 'Stack: R, Shiny and Leaflet',
+    img: '../../assets/Images/overview-images/rideshare.gif',
+    caption:
+      'A shiny app used to identify profitable areas and times for rideshare drivers.  The app was created with R programming language, leaflet and Shiny.',
+    routeLink: '/projects/r-projects',
+    IsGitHubButton: false,
+    IsDemoButton: true,
   },
   {
-    title:"RideShare App",
-    subtitle:"Stack: R, Shiny and Leaflet",
-    img: "../../assets/Images/overview-images/rideshare Project.jpg",
-    caption: "A shiny app used to identify profitable areas and times for rideshare drivers.  The app was created with R programming language, leaflet and Shiny.",
-    routeLink:"/projects/r-projects",
-    IsGitHubButton:false,
+    title: 'Invoice App',
+    subtitle: 'Stack: Angular, Sql Server and Entity Framework',
+    img: '../../assets/Images/overview-images/invoice.gif',
+    caption:
+      'An Invoice application that allows the user to create customers and invoices. The invoices can be downloaded as a pdf and can be marked as paid.',
+    routeLink: '/projects/invoice-app',
+    gitHubLink: 'https://github.com/dan933/dna-invoice-application-public',
+    IsGitHubButton: true,
   },
   {
-    title:"Picture Filter",
-    subtitle:"Stack: C",
-    img: "../../assets/Images/overview-images/C-example-sepia.jpg",
-    caption: "A C application that takes a photo and adds a filter to the picture.",
-    routeLink:"/projects/c-projects/picture",
-    IsGitHubButton:false,
+    title: 'Picture Filter',
+    subtitle: 'Stack: C',
+    img: '../../assets/Images/overview-images/C-example-sepia.jpg',
+    caption:
+      'A C application that takes a photo and adds a filter to the picture.',
+    routeLink: '/projects/c-projects/picture',
+    IsGitHubButton: false,
   },
   {
-    title:"Access Invoice relational database",
-    subtitle:"Stack: MS Access",
-    img: "../../assets/Images/overview-images/Access Example.jpg",
-    caption: "A Microsoft Access application to manage a company’s invoices.",
-    routeLink:"/projects/sql-projects",
-    IsGitHubButton:false,
+    title: 'Access Invoice relational database',
+    subtitle: 'Stack: MS Access',
+    img: '../../assets/Images/overview-images/Access Example.jpg',
+    caption: 'A Microsoft Access application to manage a company’s invoices.',
+    routeLink: '/projects/sql-projects',
+    IsGitHubButton: false,
   },
   {
-    title:"DNA Match",
-    subtitle:"Stack: Python",
-    img: "../../assets/Images/overview-images/DNA Example.png",
-    caption: "A Python program that returns the name of a person when there is a DNA match.",
-    routeLink:"/projects/python-projects",
-    IsGitHubButton:false,
-  }
-]
+    title: 'DNA Match',
+    subtitle: 'Stack: Python',
+    img: '../../assets/Images/overview-images/DNA Example.png',
+    caption:
+      'A Python program that returns the name of a person when there is a DNA match.',
+    routeLink: '/projects/python-projects',
+    IsGitHubButton: false,
+  },
+];
