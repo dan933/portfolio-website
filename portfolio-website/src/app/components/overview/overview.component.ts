4f38fdd6324bc423d8bbb295712d20f9d07b5b83
dan933 <69749413+dan933@users.noreply.github.com>
2022-09-14 06:00:18 +1000

    Added Video about me video

--- portfolio-website/src/app/components/overview/overview.component.ts
+++ portfolio-website/src/app/components/overview/overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { cardData } from 'src/assets/Data/overview-card-data';
 
 interface ICard {
@@ -17,10 +17,25 @@ interface ICard {
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss'],
 })
+
+//source https://stackblitz.com/edit/angular-rw7xf4?file=src%2Fapp%2Fapp.component.css,src%2Fapp%2Fapp.component.html,src%2Fapp%2Fapp.component.ts
 export class OverviewComponent implements OnInit {
   cards: ICard[] = cardData;
+  @ViewChild('aboutMeVideo') myVideo!: ElementRef;
+  isPlay = false;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
+
+  toggleVideo() {
+    console.log(this.myVideo.nativeElement)
+    if (this.isPlay) {
+      this.myVideo.nativeElement.pause();
+    } else {
+      this.myVideo.nativeElement.play();
+    }
+    this.isPlay = !this.isPlay;
+
+  }
 }
